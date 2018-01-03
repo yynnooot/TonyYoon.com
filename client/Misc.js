8f0ab38d08710dcab57127f913814ecca413d3c7
Tony Yoon <tooonnyy@gmail.com>
2018-01-03 16:20:12 -0500

    misc component added

--- client/Misc.js
+++ client/Misc.js
@@ -10,9 +10,25 @@ export default class Misc extends Component {
   render(){
     return(
       <div className="components" id="misc-container">
-        <h1>MISC COMPONENT</h1>
+        <h1>MISCELLANIOUS</h1>
+        <div id="illustration-div">
+          <div className="misc-column">
+            <img className="column-item" src="/assets/misc-deer.jpg" alt=""/>
+            <img className="column-item" src="/assets/misc-rhino.jpg" alt=""/>
+            <img className="column-item" src="/assets/misc-doge.jpg" alt=""/>
+          </div>
+          <div className="misc-column">
+          <img className="column-item" src="/assets/misc-rhino.jpg" alt=""/>
+          <img className="column-item" src="/assets/misc-doge.jpg" alt=""/>
+          <img className="column-item" src="/assets/misc-deer.jpg" alt=""/>
+          </div>
+          <div className="misc-column">
+          <img className="column-item" src="/assets/misc-deer.jpg" alt=""/>
+          <img className="column-item" src="/assets/misc-doge.jpg" alt=""/>
+          <img className="column-item" src="/assets/misc-rhino.jpg" alt=""/>
+          </div>
+        </div>
       </div>
-
     )
   }
 }
--- client/Ux.js
+++ client/Ux.js
@@ -21,15 +21,15 @@ export default class Ux extends Component {
         <div id="huddleinsights-container" className="project-container">
           <div className="project-header">
             <h2>Huddle Insights</h2>
-            <p>My process is about gaining an understanding of the big picture, identifying goals and pain-points, and taking a User-Centered approach in designing usable solutions. The methods and techniques I use within each phase of my process may vary depending on the design challenges.</p>
+            <p>Huddle Insights, a SaaS platform by Value Stream Labs, is a social data sharing platform. It allows market analysts, consultants, and researchers to join "huddles" and share data and insights with other members. Our team was tasked with creating wireframes for the huddle experience.</p>
             <img id="huddle-img" src="/assets/huddle-page.png" alt="huddleinsights-wireframe"/>
           </div>
         </div>
         <div id="heartbeat-container" className="project-container">
           <div className="project-header">
             <h2>HeartBeat</h2>
-            <p>My process is about gaining an understanding of the big picture, identifying goals and pain-points, and taking a User-Centered approach in designing usable solutions. The methods and techniques I use within each phase of my process may vary depending on the design challenges.</p>
-            <img src="" alt="my-ux-process"/>
+            <p>HeartBeat is an application for the Apple Watch. It is designed for patients with pre-existing heart conditions, and allows users to monitor their heart activity while recording any symptoms they may experience. The data collected can then be used by their physicians in providing best possible care.</p>
+            <img src="assets/ux-heartbeat.png" alt="my-ux-process"/>
           </div>
         </div>
       </div>
