7e1cd37e019a45196bd1d4a5f097ec14a82d8f58
Tony Yoon <tooonnyy@gmail.com>
2018-03-19 19:05:40 -0400

    added / to path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, '/'),
     filename: './public/bundle.js' // assumes your bundle.js will also be in the root of your project folder
   },
   devtool: 'source-map',
