229ed3cff7a94cc64e369160921179672dba039c
Tony Yoon <tooonnyy@gmail.com>
2018-03-19 19:00:38 -0400

    changed path in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: __dirname,
     filename: './public/bundle.js' // assumes your bundle.js will also be in the root of your project folder
   },
   devtool: 'source-map',
