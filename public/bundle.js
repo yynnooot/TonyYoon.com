71046f4e8861cb39ac80ebb8d66d4309ed77fec8
Tony Yoon <tooonnyy@gmail.com>
2018-04-27 01:26:16 -0400

    added react-native project

--- public/bundle.js
+++ public/bundle.js
@@ -36223,20 +36223,19 @@ var Dev = function (_Component) {
     var _this = _possibleConstructorReturn(this, (Dev.__proto__ || Object.getPrototypeOf(Dev)).call(this));
 
     _this.state = {
+      travelpedia: {
+        title: "Travelpedia",
+        description: "Travelpedia is a mobile app built with React-Native, Redux, and Expo. It allows users to get information about historical landmarks and architecture through images they take or have stored on their smart phones. Travelpedia utilizes Google Cloud Vision API for image recognition and Wikipedia to retrieve insightful information about the landmark.",
+        images: ['/assets/travel_mobile.png', '/assets/travel-eiffel.png'],
+        body: "How it works: I use Expo's built-in asynchronous functions to obtain a Base-64 string from an image. I then send the base-64 to Google Cloud Vision API in a Post request using axios - if image recognition is successful, Google responds with a JSON object which I parse to obtain the title of the landmark within the image. After receiving name of the landmark, I query the Wikipedia API to get summary description of the landmark. I also built a simpler web application where Users can choose images from their computer. This was built with Node, Express, React, and SCSS.",
+        route: "https://github.com/yynnooot/TravelpediaMobile"
+      },
       codecombat: {
         title: "Code Combat",
         description: 'Our team noticed that the learning resources available to new programmers were not very engaging. So we wanted to create an application that provides a fun experience for new and seasoned programmers alike. We built CodeCombat, an application where developers can improve their coding skills through solo training challenges, or live coding battles - where Users of similar rank go head-to-head and battle for points and rank.',
         images: ['/assets/codecombat-landing.png', '/assets/codecombat3.png'],
-        body: "I was project manager and led UI Development. We used React.js, SCSS, Redux and PostgreSQL for our models. For Users to type their code, we used Ace Editor and sent the code through Sockets.io to our backend Sandbox environment (VM2) to safely run user submitted code.",
+        body: "I served as project manager and led UI Development. We utitlized such technologies as React, Redux, Node, Express and PostgreSQL for our database. For Users to type their code, we used Ace Editor, and sent the user-submitted code to our back-end through websockets (Sockets.io). In order to safely run user-submitted code, we utilized VM2 to create a virtual sandbox environment.",
         route: "http://code-combat.herokuapp.com/"
-      },
-
-      travelpedia: {
-        title: "Travelpedia",
-        description: "Travelpedia is a web app that allows users to get information about historical landmarks and architecture through images. Travelpedia utilizes Google Cloud Vision API for image recognition and Wikipedia for landmark data. This was a 4 day hackathon project I built with Node, React, Express, and CSS.",
-        images: ['/assets/travelpedia-gug.png', '/assets/travel-eiffel.png'],
-        body: "Here I take an image and convert it into a base-64 string which is used to make an axios post request to Google. Google response is in JSON, and I parsed the object to extract the name of the landmark. After receiving name of the landmark, I query the Wikipedia API to get summary description of the landmark.",
-        route: "https://github.com/yynnooot/Travelpedia"
       }
     };
     return _this;
@@ -36258,8 +36257,8 @@ var Dev = function (_Component) {
             '.'
           )
         ),
-        _react2.default.createElement(_DevProject2.default, { project: this.state.codecombat }),
-        _react2.default.createElement(_DevProject2.default, { project: this.state.travelpedia })
+        _react2.default.createElement(_DevProject2.default, { project: this.state.travelpedia }),
+        _react2.default.createElement(_DevProject2.default, { project: this.state.codecombat })
       );
     }
   }]);
